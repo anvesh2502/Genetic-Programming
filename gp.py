9b6175494df2f29a70a7ceb187a2bc1d24cfed86
anvesh2502 <anveshrocks@yahoo.co.in>
2016-08-05 20:58:24 -0500

    Added example tree

--- gp.py
+++ gp.py
@@ -14,9 +14,9 @@ class node :
      def __init__(self,fw,children) :
          self.function=fw.function
          self.name=fw.name
-         self.children=fw.children
+         self.children=children
 
-    def evaluate(self,inp) :
+     def evaluate(self,inp) :
         results=[n.evaluate(inp) for n in self.children]
         return self.function(results)
 
@@ -35,3 +35,32 @@ class constnode :
 
     def evaluate(self,inp) :
         return self.v
+
+addw=fwrapper(lambda l : l[0]+l[1],2,'add')
+subw=fwrapper(lambda l : l[0]-l[1],2,'subtract')
+mulw=fwrapper(lambda l : l[0]*l[1],2,'multiply')
+
+def iffunc(l) :
+
+    if l[0]>0 : return l[1]
+    else : return l[2]
+
+ifw=fwrapper(iffunc,3,'if')
+
+def isgreater(l) :
+
+    if l[0]>l[1] : return 1
+    else : return 0
+
+gtw=fwrapper(isgreater,2,'isgreater')
+
+flist=[addw,mulw,ifw,gtw,subw]
+
+
+def exampletree() :
+
+    return node(ifw,[ node(gtw,[paramnode(0),constnode(3)]),
+    node(addw,[paramnode(1),constnode(5)]),
+    node(subw,[paramnode(1),constnode(2)]),
+    ]
+    )
